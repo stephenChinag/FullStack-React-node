4ac80e0b9e9e596a91276bdbf5eb3b1933b1d046
StpehenChinag <stephenchinag@gmail.com>
2023-10-31 13:17:57 +0100

    tanstack

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,7 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
@@ -9,7 +9,7 @@ export default function NewEvent() {
   function handleSubmit(formData) {}
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
         <>
           <Link to="../" className="button-text">
